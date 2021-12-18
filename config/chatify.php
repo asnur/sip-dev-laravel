e21705c625ba68df2035c84b36ba6236f965490b
Asnur Ramdani <asnurramdhani12@gmail.com>
2021-12-18 22:12:14 +0700

    update view and controller chatify

--- config/chatify.php
+++ config/chatify.php
@@ -7,7 +7,7 @@ return [
     | Messenger display name
     |-------------------------------------
     */
-    'name' => env('CHATIFY_NAME', 'Chatify Messenger'),
+    'name' => env('CHATIFY_NAME', 'Konsultasi'),
 
     /*
     |-------------------------------------
@@ -17,7 +17,7 @@ return [
     'routes' => [
         'prefix' => env('CHATIFY_ROUTES_PREFIX', 'chat'),
         'middleware' => env('CHATIFY_ROUTES_MIDDLEWARE', ['web', 'auth']),
-        'namespace' => env('CHATIFY_ROUTES_NAMESPACE', 'Chatify\Http\Controllers'),
+        'namespace' => env('CHATIFY_ROUTES_NAMESPACE', 'App\Http\Controllers\vendor\Chatify'),
     ],
 
     /*
--- resources/views/vendor/Chatify/layouts/favorite.blade.php
+++ resources/views/vendor/Chatify/layouts/favorite.blade.php
@@ -1,6 +1,6 @@
 <div class="favorite-list-item">
-    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m" 
-        style="background-image: url('{{ asset('/storage/'.config('chatify.user_avatar.folder').'/'.$user->avatar) }}');">
+    <div data-id="{{ $user->id }}" data-action="0" class="avatar av-m"
+        style="background-image: url('{{ asset('/storage/' . config('chatify.user_avatar.folder') . '/e7f8ef25-cb57-4d04-a1f8-ef993afb2147.png') }}');">
     </div>
-    <p>{{ strlen($user->name) > 5 ? substr($user->name,0,6).'..' : $user->name }}</p>
+    <p>{{ strlen($user->name) > 5 ? substr($user->name, 0, 6) . '..' : $user->name }}</p>
 </div>
