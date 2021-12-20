6198282eebc8487df6420e5ad213317381f88d43
Fahmii19 <fhmialfi79@gmail.com>
2021-12-20 22:13:26 +0700

    push merge new

--- public/assets/js/mobile.js
+++ public/assets/js/mobile.js
@@ -1,5 +1,9 @@
-var url = "https://jakpintasdev.dpmptsp-dki.com:3000/";
-var clickEvent = "touchstart";
+var url = "https://jakpintas.dpmptsp-dki.com:3000/";
+
+// var clickEvent = "touchstart";
+var clickEvent =
+    "ontouchstart" in document.documentElement ? "touchstart" : "click";
+
 mapboxgl.accessToken =
     "pk.eyJ1IjoibWVudGhvZWxzciIsImEiOiJja3M0MDZiMHMwZW83MnVwaDZ6Z2NhY2JxIn0.vQFxEZsM7Vvr-PX3FMOGiQ";
 const map = new mapboxgl.Map({
