b42db02f4719c1f8f38f98e6e085c49009316633
Fahmii19 <fhmialfi79@gmail.com>
2022-01-10 23:18:24 +0700

    push

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1709,19 +1709,9 @@
     </div>
 </div> --}}
 
-
-
-
     <div class="hide_zoning_fill">
 
-
         <div style="position: fixed; right:6%; top:36%; color:#fff;">
-
-
-
-            {{-- <div style="position: fixed; right:1%; top:37%; color:#fff;"> --}}
-
-
 
             <div class="d-flex align-content-stretch flex-wrap justify-content-center">
 
@@ -1733,19 +1723,12 @@
 
             </div>
 
-
             {{-- <div style="margin-top: -10%;" class="col-sm-5">
 
         </div> --}}
 
-
-
-
         </div>
     </div>
-
-
-
 
     <div class="p-2 dropdown for_mobile" id="more-apps" style=" z-index:9; margin-top:4rem; margin-left:5px;">
 
