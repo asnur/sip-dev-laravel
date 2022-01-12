1185eebf8118e520c072004c06c74572d69dcb27
Fahmii19 <fhmialfi79@gmail.com>
2022-01-12 11:53:55 +0700

    fix url

--- public/assets/js/mobile.js
+++ public/assets/js/mobile.js
@@ -1,4 +1,4 @@
-var url = `${APP_URL}:3000`;
+var url = `https://jakpintas.dpmptsp-dki.com:3000`;
 // var url = `https://jakpintas.dpmptsp-dki.com:3000`;
 
 var kilometer = $("#ControlRange").val() / 1000;
