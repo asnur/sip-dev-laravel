76c4db08e96857904ffe4a41465b3f768c37c0bd
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-02-10 17:32:01 +0700

    change to cdn

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Print PDF</title>
     <style>
