a425b1a3ccc7d7f7ad2f6013f5061c8af72d20cb
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-03-09 15:36:15 +0700

    disable raster

--- public/assets/js/web.js
+++ public/assets/js/web.js
@@ -696,44 +696,44 @@ map.on("style.load", function () {
         },
         labelLayerId
     );
-    map.addSource("mapbox-dem", {
-        type: "raster-dem",
-        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-        tileSize: 512,
-        maxzoom: 14,
-    });
-    map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+    // map.addSource("mapbox-dem", {
+    //     type: "raster-dem",
+    //     url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+    //     tileSize: 512,
+    //     maxzoom: 14,
+    // });
+    // map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
 
-    map.addLayer({
-        id: "sky",
-        type: "sky",
-        paint: {
-            // set up the sky layer to use a color gradient
-            "sky-type": "gradient",
-            // the sky will be lightest in the center and get darker moving radially outward
-            // this simulates the look of the sun just below the horizon
-            "sky-gradient": [
-                "interpolate",
-                ["linear"],
-                ["sky-radial-progress"],
-                0.8,
-                "rgba(135, 206, 235, 1.0)",
-                1,
-                "rgba(0,0,0,0.1)",
-            ],
-            "sky-gradient-center": [0, 0],
-            "sky-gradient-radius": 90,
-            "sky-opacity": [
-                "interpolate",
-                ["exponential", 0.1],
-                ["zoom"],
-                5,
-                0,
-                22,
-                1,
-            ],
-        },
-    });
+    // map.addLayer({
+    //     id: "sky",
+    //     type: "sky",
+    //     paint: {
+    //         // set up the sky layer to use a color gradient
+    //         "sky-type": "gradient",
+    //         // the sky will be lightest in the center and get darker moving radially outward
+    //         // this simulates the look of the sun just below the horizon
+    //         "sky-gradient": [
+    //             "interpolate",
+    //             ["linear"],
+    //             ["sky-radial-progress"],
+    //             0.8,
+    //             "rgba(135, 206, 235, 1.0)",
+    //             1,
+    //             "rgba(0,0,0,0.1)",
+    //         ],
+    //         "sky-gradient-center": [0, 0],
+    //         "sky-gradient-radius": 90,
+    //         "sky-opacity": [
+    //             "interpolate",
+    //             ["exponential", 0.1],
+    //             ["zoom"],
+    //             5,
+    //             0,
+    //             22,
+    //             1,
+    //         ],
+    //     },
+    // });
     map.on(clickEvent, function (e) {
         // console.log(e);
         const coornya = e.lngLat;
