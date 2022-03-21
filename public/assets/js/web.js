0c3335d1a803f3a6c618fcc816d712eb4fc90cbd
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-03-21 10:38:19 +0700

    update proyek potensial

--- public/assets/js/web.js
+++ public/assets/js/web.js
@@ -1053,7 +1053,9 @@ map.on("mouseenter", "investasi_fill", (e) => {
     const content = `<div class="card">
     <div class="card-body p-2">
       <h6 class="mt-0 mb-2 card-title border-bottom">${dt["Nama"]}</h6>
-      <span class="d-block" style="width: 300px"><b>Deskripsi :</b> ${dt["Deskripsi"]}</span>
+      <span class="d-block" style="width: 300px"><b>Rencana Proyek :</b> ${dt["Rencana_Proyek"]}</span>
+      <span class="d-block" style="width: 300px"><b>Luas Lahan :</b> ${dt["Luas_Lahan"]}</span>
+      <span class="d-block" style="width: 300px"><b>Total Investasi :</b> ${dt["Total_Investasi"]}</span>
 
 
     </div>`;
@@ -1081,7 +1083,9 @@ map.on("mouseenter", "investasi_line", (e) => {
     <div class="card-body p-2">
       <h6 class="mt-0 mb-2 card-title border-bottom">${dt["Nama"]}</h6>
       <div style="line-height: 1.2;">
-      <span class="d-block" style="width: 300px"><b>Deskripsi :</b> ${dt["Deskripsi"]}</span>
+      <span class="d-block" style="width: 300px"><b>Rencana Proyek :</b> ${dt["Rencana_Proyek"]}</span>
+      <span class="d-block" style="width: 300px"><b>Luas Lahan :</b> ${dt["Luas_Lahan"]}</span>
+      <span class="d-block" style="width: 300px"><b>Total Investasi :</b> ${dt["Total_Investasi"]}</span>
     </div>`;
 
     // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
@@ -1107,7 +1111,9 @@ map.on("mouseenter", "investasi_dot", (e) => {
     <div class="card-body p-2">
       <h6 class="mt-0 mb-2 card-title border-bottom">${dt["Nama"]}</h6>
       <div style="line-height: 1.2;">
-      <span class="d-block" style="width: 300px"><b>Deskripsi :</b> ${dt["Deskripsi"]}</span>
+      <span class="d-block" style="width: 300px"><b>Rencana Proyek :</b> ${dt["Rencana_Proyek"]}</span>
+      <span class="d-block" style="width: 300px"><b>Luas Lahan :</b> ${dt["Luas_Lahan"]}</span>
+      <span class="d-block" style="width: 300px"><b>Total Investasi :</b> ${dt["Total_Investasi"]}</span>
     </div>`;
 
     // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
@@ -3269,8 +3275,38 @@ function onOffLayers() {
                 ) {
                     content += `
                         <li class="item mb-3" style="margin-left:-20px">
-                            <span style="font-size: 11pt" class="font-weight-bold">${infoProyek[index]["properties"]["Nama"]}</span>
-                            <label style="font-size: 13px;">${infoProyek[index]["properties"]["Deskripsi"]}</label>
+                            <span style="font-size: 11pt" class="font-weight-bold">${
+                                infoProyek[index]["properties"]["Nama"]
+                            }</span>
+                            <ol type="a">
+                                <li style="font-size:13px;margin-left:-1.6rem"><b>Rencana Proyek</b> : ${
+                                    infoProyek[index]["properties"][
+                                        "Rencana_Proyek"
+                                    ] == ""
+                                        ? "-"
+                                        : infoProyek[index]["properties"][
+                                              "Rencana_Proyek"
+                                          ]
+                                }</li>
+                                <li style="font-size:13px;margin-left:-1.6rem"><b>Luas Lahan</b> : ${
+                                    infoProyek[index]["properties"][
+                                        "Luas_Lahan"
+                                    ] == ""
+                                        ? "-"
+                                        : infoProyek[index]["properties"][
+                                              "Luas_Lahan"
+                                          ]
+                                }</li>
+                                <li style="font-size:13px;margin-left:-1.6rem"><b>Total Investasi</b> : ${
+                                    infoProyek[index]["properties"][
+                                        "Total_Investasi"
+                                    ] == ""
+                                        ? "-"
+                                        : infoProyek[index]["properties"][
+                                              "Total_Investasi"
+                                          ]
+                                }</li>
+                            </ol>
                         </li>
                     `;
                 }
