2478ac100c312a13de0eecaa906c3961b0ac4a8e
Fahmii19 <fhmialfi79@gmail.com>
2022-07-04 15:51:10 +0700

    revisi login hilangkan efek btn

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5806,7 +5806,7 @@ $option_simulasi = ['Rumah Mewah', 'Rumah Biasa', 'Apartemen', 'Rumah Susun', 'A
 
     {{-- ScreenShoot Map --}}
     <div id="screenshotPlaceholder"></div>
-    <div class="runing-text" data-duplicated='true' data-direction='left'>-</div>
+    <div class="runing-text" data-duplicated='false' data-direction='left'>-</div>
 
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
