a3d57d77117b09bc7e295a89bf6118d011488ca7
Fahmii19 <fhmialfi79@gmail.com>
2022-07-05 17:05:26 +0700

    upload video

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="flex flex-row min-h-screen">
     <div class="w-9/12">
-        <video class="object-fill w-full h-full" src="{{ asset('assets/login/bg_video.mp4') }}" type="video/mp4" autoplay muted loop></video>
+        <video class="object-fill w-full h-full" src="{{ asset('login/video1.mp4') }}" type="video/mp4" autoplay muted loop></video>
     </div>
     <div class="w-3/12 gradient_card">
         <div class="flex flex-col h-screen">
             <div class="flex flex-row items-center justify-center bg-white">
                 <div class="flex items-center justify-center mr-6 mb-1">
-                    <img class="" style="width: 100%" src="{{ asset('assets/login/logo_jakpintas.png') }}" alt="" />
+                    <img class="" style="width: 100%" src="{{ asset('login/logo_jakpintas.png') }}" alt="" />
                 </div>
                 <div class="flex flex-col items-center justify-center text-center mb-2 py-2">
                     <div class="w-50">
