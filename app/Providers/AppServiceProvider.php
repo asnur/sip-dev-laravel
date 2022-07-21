e71a48b022840c33b95ee3e3a77145cd7117c20d
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-07-21 13:00:54 +0700

    change timezone with boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config('app.timezone', 'Asia/Jakarta');
+        config('app.locale', 'id');
     }
 }
